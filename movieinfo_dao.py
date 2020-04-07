ec9588a13fa7dbc3ffdfc9eb24761d044bbb1ba1
unknown <gshobhit033@gmail.com>
2020-04-07 23:36:48 +0530

    adding

--- movieinfo_dao.py
+++ movieinfo_dao.py
@@ -22,38 +22,42 @@ class MovieinfoDao:
 
 		options=''
 		today = datetime.today()
+		tommorow=today+timedelta(days=1)
 		week_ago = today - timedelta(days=7)
 		week_next=today + timedelta(days=7)
 		if inTheatre:
 			options=[week_ago.date(), today.date()]
 		if comingSoon:
-			options=[today.date(),week_next.date()]
+			options=[tommorow,week_next.date()]
 		output = self.connection.run(query,False,options)
 		list=[]
-		for record in output:
-			id=record[0]
-			title = record[1]
-			year = record[2]
-			Rated = record[3]
-			Released = record[4].strftime('%Y-%m-%d')
-			runtime  = record[5]
-			_Genre  = self.genreDao.fetchGenreByMovieinfoId(id)
-			_Director = self.directorDao.fetchDirectorByMovieinfoId(id)
-			_Writer = self.writerDao.fetchWriterByMovieinfoId(id)
-			_Actor = self.actorDao.fetchActorByMovieinfoId(id)
-			Plot = record[10]
-			Language = record[11]
-			Country = record[12]
-			_Awards = record[13]
-			Poster = record[14]
-			rating = record[15]
-			votes = record[16] 
-			Type = record[17]
-			BoxOffice = record[18]
-			Production = record[19]
-			movieinfoObj = Movieinfo(id,title,year,Rated,Released,runtime,_Genre,_Director,
-			_Writer,_Actor,Plot,Language,Country,_Awards,Poster,rating,votes,Type,BoxOffice,Production)
-			list.append(movieinfoObj)
+		if output:
+			for record in output:
+				id=record[0]
+				title = record[1]
+				year = record[2]
+				Rated = record[3]
+				Released = record[4].strftime('%Y-%m-%d')
+				runtime  = record[5]
+				_Genre  = self.genreDao.fetchGenreByMovieinfoId(id)
+				_Director = self.directorDao.fetchDirectorByMovieinfoId(id)
+				_Writer = self.writerDao.fetchWriterByMovieinfoId(id)
+				_Actor = self.actorDao.fetchActorByMovieinfoId(id)
+				Plot = record[10]
+				Language = record[11]
+				Country = record[12]
+				_Awards = record[13]
+				Poster = record[14]
+				rating = record[15]
+				votes = record[16] 
+				Type = record[17]
+				BoxOffice = record[18]
+				Production = record[19]
+				movieinfoObj = Movieinfo(id,title,year,Rated,Released,runtime,_Genre,_Director,
+				_Writer,_Actor,Plot,Language,Country,_Awards,Poster,rating,votes,Type,BoxOffice,Production)
+				list.append(movieinfoObj)
+		else:
+			return []
 		
 		return list
 
