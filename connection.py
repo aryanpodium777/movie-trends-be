7eecc2e37054558b1c01a11d5f866766e21649b6
unknown <gshobhit033@gmail.com>
2020-04-18 16:49:43 +0530

    added remote db configuration

--- connection.py
+++ connection.py
@@ -1,20 +1,26 @@
 import mysql.connector
 from singleton import Singleton
 
+
 class Connection(metaclass=Singleton):
+
+    host = "db4free.net"
+    user = "aryangupta"
+    password = "mtbbd123"
+
     def __init__(self):
         self.db = mysql.connector.connect(
-            host="localhost",
+            host=self.host,
             port=3306,
-            user="root",
-            password="",
+            user=self.user,
+            password=self.password,
             db="movie_trends"
-            )
+        )
 
-    def run(self,query,isSingle,options=[],isFetchQuery=True):
+    def run(self, query, isSingle, options=[], isFetchQuery=True):
         try:
             cursor = self.db.cursor()
-            cursor.execute(query,options)
+            cursor.execute(query, options)
             if isFetchQuery:
                 if isSingle:
                     result = cursor.fetchone()
@@ -24,7 +30,4 @@ class Connection(metaclass=Singleton):
             else:
                 return 1
         except Exception as e:
-            print(e,'-------exception------') 
-            
-         
-                      
+            print(e, '-------exception------')
