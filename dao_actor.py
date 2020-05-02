d9374ae53602ddf2770e915d7a8aa06612292ccc
unknown <gshobhit033@gmail.com>
2020-05-02 16:59:55 +0530

    bar change

--- dao_actor.py
+++ dao_actor.py
@@ -52,7 +52,8 @@ class ActorDao(metaclass=Singleton):
 		list = []
 		for record in output:
 			year = record[0],
-			box_office_collection = record[1]
+			year = year[0]
+			box_office_collection = str(record[1])
 			actorObj = AnalyticsBar(year,box_office_collection)
 			list.append(actorObj)
 		return list
